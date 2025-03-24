e2923a357a8f0fcffdb9640efdd4d395e7f2dc50
Liviu Ghita <liviu.ghita@sicor.de>
2025-03-24 09:25:59 +0100

    [WIP]

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+)
+
+func run(ctx context.Context, w io.Writer, args []string) error {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	// ...
+	return nil
+}
+
 func main() {
-	print("wip")
+	ctx := context.Background()
+	if err := run(ctx, os.Stdout, os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
 }
